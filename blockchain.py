4f97474736e408635e50514fb7252333ef877d06
Finlay Campbell <43293464+rootfinlay@users.noreply.github.com>
2022-11-03 15:53:35 +0000

    Update blockchain.py
    Remove POW algorithm from blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -114,21 +114,6 @@ def blkHashScript(blk_content):
     print(hashed_output)
     return hashed_output
 
-def proofOfWork(level=5):
-    proof_count = 0
-    prototype = random_prototype()
-    h = ''
-    while True:
-        for pow in range(0, 512):
-            temp_string = ''.join(prototype)
-            temp_string = temp_string.join(str(pow+random.randint(1,4096)*random.randint(1,4096)))
-            h = hashlib.sha256(bytes(temp_string, encoding='utf-8')).hexdigest()
-            proof_count += 1
-            if h[0:level] == ('0' * level):
-                print("Proof done")
-                proof_of_work = h
-                return proof_of_work
-
 def random_prototype():
     return ''.join([random.choice(string.ascii_letters) for n in range(16)])
 
